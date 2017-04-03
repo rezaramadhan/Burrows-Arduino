0a9ea2a26761552a647364ab46e66ffb13714398
Muhammad Reza Ramadhan <rezaramadhan.m@gmail.com>
2017-04-03 12:10:06 +0700

    Add counter life 7segment to main program

--- main/main.ino
+++ main/main.ino
@@ -1,20 +1,14 @@
-#define a 4
-#define b 5
-#define c A3
-#define d A4
-#define e A5
-#define f 3
-#define g 2
-
-void showDigit(int digit){
-  
-}
-
+#define DATA_PIN 2
+#define CLOCK_PIN 3
+#define MAX_IDLE_SEC 5
 #include <LiquidCrystal.h>
 
 // initialize the library with the numbers of the interface pins
-//LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
 LiquidCrystal lcd(8,9,10,11,12,13);
+
+byte display[] = {B10000001, B11101101, B01000011, B01001001, B00101101, B00011001};
+int countdown = MAX_IDLE_SEC;
+int iter = 1;
 
 void setup() { 
   lcd.begin(16, 2);
@@ -27,75 +21,57 @@ void setup() {
   pinMode(A1,INPUT);
   pinMode(A2,INPUT);
 
-  pinMode(2,OUTPUT);
-  pinMode(3,OUTPUT);
-  pinMode(4,OUTPUT);
-  pinMode(5,OUTPUT);
-  pinMode(A3,OUTPUT);
-  pinMode(A4,OUTPUT);
-  pinMode(A5,OUTPUT);
+  pinMode(CLOCK_PIN, OUTPUT);
+  pinMode(DATA_PIN, OUTPUT);
   
-  
+  shiftOut(DATA_PIN, CLOCK_PIN, LSBFIRST, display[countdown]);
 }
 
 void loop() {
+
+  // Write Score to LCD after reading from serial communication
   if(Serial.available() > 0){
     lcd.setCursor(0, 1);
     lcd.print(Serial.readString());  
   }
-  
+
+
+  // read from LDR, send to unity
   int LDR1 = analogRead(A0);
   int LDR2 = analogRead(A1);
   int LDR3 = analogRead(A2);
-//  Serial.print(LDR1);
-//  Serial.print(" ");
-//  Serial.print(LDR2);
-//  Serial.print(" ");
-//  Serial.println(LDR3);
-//  delay(500);
-  if (LDR1 <= 200 && LDR2 <= 200 && LDR3 <= 200) {
-    Serial.write(0);
-    //Serial.println('0');
-  }
-
-  else if (LDR1 > 200 && LDR2 <= 200 && LDR3 <= 200) {
-    Serial.write(1);
-    //Serial.println('1');
-  }
-
-  else if (LDR1 <= 200 && LDR2 > 200 && LDR3 <= 200) {
-    Serial.write(2);
-    //Serial.println('2');
-  }
-
-  else if (LDR1 <= 200 && LDR2 <= 200 && LDR3 > 200) {
-    Serial.write(3);
-    //Serial.println('3');
-  }
-
-  else if (LDR1 > 200 && LDR2 > 200 && LDR3 <= 200) {
-    Serial.write(4);
-    //Serial.println('4');
-  }
-
-  else if (LDR1 > 200 && LDR2 <= 200 && LDR3 > 200) {
-    Serial.write(5);
-    //Serial.println('5');
-  }
-
-  else if (LDR1 <= 200 && LDR2 > 200 && LDR3 > 200) {
-    Serial.write(6);
-    //Serial.println('6');
-  }
-  
-  else if (LDR1 > 200 && LDR2 > 200 && LDR3 > 200) {
+  if (LDR1 > 200 && LDR2 > 200 && LDR3 > 200) {
     Serial.write(7);
-    //Serial.println('7');
+    iter++;
+  } else {
+    iter = 0;
+    if (LDR1 > 200 && LDR2 <= 200 && LDR3 <= 200) {
+      Serial.write(1);
+    } else if (LDR1 <= 200 && LDR2 > 200 && LDR3 <= 200) {
+      Serial.write(2);
+    } else if (LDR1 <= 200 && LDR2 <= 200 && LDR3 > 200) {
+      Serial.write(3);
+    } else if (LDR1 > 200 && LDR2 > 200 && LDR3 <= 200) {
+      Serial.write(4);
+    } else if (LDR1 > 200 && LDR2 <= 200 && LDR3 > 200) {
+      Serial.write(5);
+    } else if (LDR1 <= 200 && LDR2 > 200 && LDR3 > 200) {
+      Serial.write(6);
+    } else if (LDR1 <= 200 && LDR2 <= 200 && LDR3 <= 200) {
+      Serial.write(0);
+    }
   }
   delay(20);
 
-//    for(int i = 0; i < 10; i++ ){
-//      showDigit(i);
-//      delay(500);
-//    }
+  if (iter == 0) {
+    countdown = MAX_IDLE_SEC;
+    shiftOut(DATA_PIN, CLOCK_PIN, LSBFIRST, display[countdown]);  
+  } else if (iter >= 50) {
+    countdown--;
+    if (countdown < 0) {
+      countdown = MAX_IDLE_SEC;
+    }
+    shiftOut(DATA_PIN, CLOCK_PIN, LSBFIRST, display[countdown]);
+    iter = 1;
+  }
 }
